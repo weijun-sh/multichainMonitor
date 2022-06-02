ef9e3b7aaa3882923807df7357d2fe3edcd3fb1b
DannyZhang1991 <13026610069@163.com>
2022-06-02 09:11:07 +0800

    py2

--- ugli.py
+++ ugli.py
@@ -1,10 +1,17 @@
 import os
 import sys
 def uglifyjsFile (file):
-    uglifyjsCmd = 'node_modules/.bin/uglifyjs ' + file + ' -m -o ' + file
-    if os.system(uglifyjsCmd) != 0:
-        print('uglifyjs fail!')
-        sys.exit(1)
+    try :
+        uglifyjsCmd = 'node_modules/.bin/uglifyjs ' + file + ' -m -o ' + file
+        if os.system(uglifyjsCmd) != 0:
+            print('uglifyjs fail!')
+        else:
+            print('success')
+    except Exception as e :
+        print('error')
+    finally :
+        print('finish')
+
 
 def uglifyjsDir (customizeConfigPath):
     for root,dirs,files in os.walk(customizeConfigPath):
@@ -16,4 +23,4 @@ if os.system('npm install uglify-es') != 0:
     print('npm install uglifyjs fail!')
     sys.exit(1)
 
-uglifyjsDir ('/opt/grafana/grafana/public/build/')
+uglifyjsDir ('/opt/grafana/grafana/public/build')
