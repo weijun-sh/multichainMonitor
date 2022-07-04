e1c048446e8b9781222a49bf69ad25776f0cf54d
DannyZhang1991 <13026610069@163.com>
2022-07-04 09:35:37 +0800

    rm

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var app = express();
 var axios = require('axios');
 var bodyParser = require('body-parser')
 app.use(express.static(path.join(__dirname, 'dist')));
-let proxyPrometheus = require('./proxy-prometheus')
 const viewsPath = path.join(__dirname, 'views');
 app.set('views', viewsPath);
 app.set('view engine', 'ejs');
@@ -18,6 +17,5 @@ const getAccess = () => {
   return access;
 }; // 代码中会兼容本地 service mock 以及部署站点的静态数据
 
-proxyPrometheus.init(app).proxy()
 
 app.listen(20520, function () {});
