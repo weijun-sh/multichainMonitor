cbc6ba7aebeb63da4fb24de3bc6c99b475c66e93
DannyZhang1991 <13026610069@163.com>
2022-08-30 16:59:46 +0800

    first time

--- index.js
+++ index.js
@@ -31,19 +31,59 @@ app.all("*", function (req, res, next) {
 app.get('/view', function (req, res){
     let filePath = path.join(__dirname, './views/arrivalTimeout.html');
 
+    let columns = [{
+        title: "#",
+        dataIndex: "rowKey",
+        key: "rowKey",
+        csvWidth: 4,
+        render: (text, record) => {
+            return record.rowKey
+        }
+    }, {
+        title: '桥或路由',
+        dataIndex: 'bridge',
+        key: 'bridge',
+        render: (text, record) => {
+            return record.bridge
+        }
+    }, {
+        title: '更新时间',
+        dataIndex: 'timestamp',
+        key: 'timestamp',
+        render: (text, record) => {
+            return record.timestamp
+        }
+    }, {
+        title: "注册时间",
+        dataIndex: 'inittime',
+        key: 'inittime',
+        render: (text, record) => {
+            return record.inittime
+        }
+    }];
+
+
+
     analysis().then(list => {
+        list = list.map((item, index) => {
+            item.rowKey = index + 1;
+            return item
+        });
+        list = list.map(record => {
+            columns.forEach(cItem => {
+                let text = cItem.render(null, record);
+                record[cItem.dataIndex] = {
+                    data: record[cItem.dataIndex],
+                    text: text
+                }
+            })
+            return record;
+        });
+        console.log("list ==>", list)
+
         renderView(filePath, {
             list,
-            columns: [{
-                title: "#",
-                dataIndex: "rowKey",
-                key: "rowKey",
-                csvWidth: 4,
-            }, {
-                title: '桥或路由',
-                dataIndex: 'bridge',
-                key: 'bridge',
-            }]
+            columns: columns
         }).then((html) => {
             res.send(html)
         })
