f0ca213cccd6fa5cc1f65624048852c41b36bc25
DannyZhang1991 <13026610069@163.com>
2022-10-24 11:55:06 +0800

    save

--- src/fileStorage/index.js
+++ src/fileStorage/index.js
@@ -2,6 +2,7 @@ const path =require("path") ;
 const {fileSaveObj, fileGetObj, fileIsExit} = require("../utils/fileSys");
 
 const systemStoragePath = path.join(__dirname, './', 'SYSTEM_STORAGE.json')
+let systemStorage = null;
 
 const INIT_DATA = {
     monitor: {
@@ -11,8 +12,6 @@ const INIT_DATA = {
 
     }
 }
-
-
 
 function systemStorageSave(obj){
     fileSaveObj(systemStoragePath, obj)
@@ -29,9 +28,18 @@ function systemStorageInit(){
 
     if(!exits){
         systemStorageSave(INIT_DATA);
+        systemStorage = INIT_DATA;
         console.log("初始化system storage")
     }else {
-        console.log("system storage 已经 初始化")
+        console.log("system storage 已经 初始化");
+        let obj = systemStorageGet();
+        if(obj){
+            systemStorage = systemStorageGet();
+        }else {
+            systemStorageSave(INIT_DATA);
+            systemStorage = INIT_DATA;
+            console.log("system storage 重新初始化 ");
+        }
     }
 }
 systemStorageInit()
